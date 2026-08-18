66b21a2305d028341feb30fee6dd4d2bd5b0b112
tanechop <achinel51@gmail.com>
2026-08-18 14:01:11 +0100

    ADD visitors_db sql

---
+++ visitors_db.sql
@@ -0,0 +1,106 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 18, 2026 at 01:44 PM
+-- Server version: 10.4.25-MariaDB
+-- PHP Version: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `visitors_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `utilisateur`
+--
+
+CREATE TABLE `utilisateur` (
+  `id_Utilisateur` int(11) NOT NULL,
+  `nom_d_utilisateur` varchar(45) NOT NULL,
+  `mot_de_passe` varchar(45) NOT NULL,
+  `role` varchar(45) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `visites`
+--
+
+CREATE TABLE `visites` (
+  `id_Visites` int(11) NOT NULL,
+  `motif` varchar(45) DEFAULT NULL,
+  `date_visite` datetime(3) DEFAULT NULL,
+  `heure_de_depart` datetime DEFAULT NULL,
+  `heure_d_arrivee` datetime DEFAULT NULL,
+  `service` varchar(45) DEFAULT NULL,
+  `Visiteurs_id_Visiteurs` int(11) NOT NULL,
+  `Utilisateur_id_Utilisateur` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `visiteurs`
+--
+
+CREATE TABLE `visiteurs` (
+  `id_Visiteurs` int(11) NOT NULL,
+  `nom` varchar(45) DEFAULT NULL,
+  `prenom` varchar(45) DEFAULT NULL,
+  `contact` int(11) DEFAULT NULL,
+  `num_CNI` varchar(45) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `utilisateur`
+--
+ALTER TABLE `utilisateur`
+  ADD PRIMARY KEY (`id_Utilisateur`);
+
+--
+-- Indexes for table `visites`
+--
+ALTER TABLE `visites`
+  ADD PRIMARY KEY (`id_Visites`),
+  ADD KEY `fk_Visites_Visiteurs_idx` (`Visiteurs_id_Visiteurs`),
+  ADD KEY `fk_Visites_Utilisateur1_idx` (`Utilisateur_id_Utilisateur`);
+
+--
+-- Indexes for table `visiteurs`
+--
+ALTER TABLE `visiteurs`
+  ADD PRIMARY KEY (`id_Visiteurs`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `visites`
+--
+ALTER TABLE `visites`
+  ADD CONSTRAINT `fk_Visites_Utilisateur1` FOREIGN KEY (`Utilisateur_id_Utilisateur`) REFERENCES `utilisateur` (`id_Utilisateur`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_Visites_Visiteurs` FOREIGN KEY (`Visiteurs_id_Visiteurs`) REFERENCES `visiteurs` (`id_Visiteurs`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
