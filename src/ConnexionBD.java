f48da751a5cdcea29556526b503d63957d88f1a2
ArnoldTokam <arnoldtokam20@gmail.com>
2026-08-20 11:25:07 +0100

    afterdark's first commit

--- src/ConnexionBD.java
+++ src/ConnexionBD.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class ConnexionBD {
+    private static final String url = "jdbc:mysql://localhost:3306/gestion_visiteurs";
+    private static final String username = "root";
+    private static final String password = "";
+    public static Connection getConnection() {
+        try {
+            Connection connection = DriverManager.getConnection(url,username,password);
+            System.out.println("Connexion réussie");
+            return connection;
+        }catch (SQLException e){
+            System.out.println("Erreur de connexion"+e.getMessage());
+            return null;
+        }
+    }
 }
+
+
