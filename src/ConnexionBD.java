9014609a646c5aff0459e79bed9c0a85bdf5e284
ArnoldTokam <arnoldtokam20@gmail.com>
2026-08-21 11:49:01 +0100

    Hachage du mot de passe et création du premier compte administrateur

--- src/ConnexionBD.java
+++ src/ConnexionBD.java
@@ -6,6 +6,7 @@ public class ConnexionBD {
     private static final String url = "jdbc:mysql://localhost:3306/gestion_visiteurs";
     private static final String username = "root";
     private static final String password = "";
+
     public static Connection getConnection() {
         try {
             Connection connection = DriverManager.getConnection(url,username,password);
