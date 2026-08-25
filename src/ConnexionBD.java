2897ec2187f0edb5759ec20f42833898f6e9a02e
237killer <139449250+237killer@users.noreply.github.com>
2026-08-25 09:40:55 +0100

    mes modifications

--- src/ConnexionBD.java
+++ src/ConnexionBD.java
@@ -2,7 +2,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class ConnexionBD {
+public class  ConnexionBD {
     private static final String url = "jdbc:mysql://localhost:3306/gestion_visiteurs";
     private static final String username = "root";
     private static final String password = "";
